161781690da90cdfc19f2a1a27a27000fa5f307c
jgriffith <jgriffith@thetus.com>
2013-11-19 12:58:59 -0800

    Styling

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ var CodeMirrorMerge = require('code-mirror/addon/merge'),
 	CodeMirror = require('code-mirror/mode/javascript'),
 	$ = require('jquery-browserify');
 
-var leftPanel = new CodeMirrorMerge.MergeView(document.getElementById('split-left'), {
+var leftPanel = new CodeMirrorMerge.MergeView(document.getElementById('git-diff'), {
 	value: $('.both').html(),
 	origLeft: $('.yours').html(),
 	origRight: $('.theirs').html(),
