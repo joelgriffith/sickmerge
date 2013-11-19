acab4e937d187ddf9e75b955ca574afed96e590c
jgriffith <jgriffith@thetus.com>
2013-11-18 16:45:42 -0800

    Small cleanup

--- sickmerge.js
+++ sickmerge.js
@@ -6,7 +6,9 @@
  *
  * Copyright (c) 2013 jgriffith
  * Licensed under the MIT license.
- *//*
+ */
+
+ /*
  * Module Dependencies
  */
 var express = require('express'),
@@ -14,43 +16,47 @@ var express = require('express'),
     port = 2001,
     fs = require('fs'),
     path = require('path'),
+    exec = require('child_process').exec,
     userArgs = process.argv.slice(2),
 	searchParam = userArgs[0],
 	gitStrip = require('./lib/gitStrip');
 
-/*
- * Application configuration and initiation
- */
-app.use(express.bodyParser());
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// Strip the passed file and build the page/service
+gitStrip(searchParam, function(err, result) {
 
-/*
- * Serve the static public directory
- */
-app.use(express.static(path.join(__dirname, 'public')));
+	if(err) throw 'There was a problem opening the file: ' + err;
 
-// Route to retrive files on the system for editing
-app.get('/', function (req, res) {
+	/*
+	 * Application configuration and initiation
+	 */
+	app.use(express.bodyParser());
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
 
-	// Strip the passed file and build the page
-	gitStrip(searchParam, function(err, result) {
-		if (err) throw 'There was a problem with loading your file, check to see if it exists: ' + err;
+	/*
+	 * Serve the static public directory
+	 */
+	app.use(express.static(path.join(__dirname, 'public')));
+
+	// Route to retrive files on the system for editing
+	app.get('/', function (req, res) {
 		res.render('editor', { body: result });
 	});
-});
 
-// Route for persisting to the file system
-app.post('/save', function (req) {
-	var location = '.' + req.body.location.replace('/file',''),
-		content = req.body.text;
+	// Route for persisting to the file system
+	app.post('/save', function (req) {
+		var location = '.' + req.body.location.replace('/file',''),
+			content = req.body.text;
 
-	fs.writeFile(location, content, function (err) {
-		if (err) throw err;
+		fs.writeFile(location, content, function (err) {
+			if (err) throw err;
+		});
 	});
-});
 
-/* 
- * Startup the server
- */
-app.listen(port);
+	/* 
+	 * Startup the server
+	 */
+	app.listen(port);
+	
+	exec('open http://localhost:' + port);
+});
