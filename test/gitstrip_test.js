9b437c38f81e3cc65fb23f7e2a9d5193c56ee2d6
jgriffith <jgriffith@thetus.com>
2013-11-18 16:45:51 -0800

    Adding in tests

---
+++ test/gitstrip_test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gitStrip = require('../lib/gitStrip.js'),
+    testData = './fixtures/test',
+    dummyFunction = function() {};
+
+exports['gitstrip'] = {
+  setUp: function(done) {
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    test.equal(gitStrip(), 'Please pass a file to parse and a callback function', 'Should show an error message.');
+    test.done();
+  },
+  'invalid file' : function(test) {
+    test.expect(1);
+    test.throws(gitStrip('notafile'), 'Should return an error when doesn\'t exist');
+    test.done();
+  },
+  'normal operation': function(test) {
+    test.expect(1);
+    test.doesNotThrow(gitStrip(testData, dummyFunction), 'Should not throw an error when a valid file is passed');
+    test.done();
+  }
+};
