bc419e2d2857ee9791bad17d7121464e560dcdf3
jgriffith <jgriffith@thetus.com>
2013-11-19 12:35:40 -0800

    Minor adjustments

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,12 @@ var CodeMirrorMerge = require('code-mirror/addon/merge'),
 	CodeMirror = require('code-mirror/mode/javascript'),
 	$ = require('jquery-browserify');
 
-	var leftPanel = new CodeMirrorMerge.MergeView(document.getElementById('split-left'), {
-		value: $('.both').html(),
-		origLeft: $('.yours').html(),
-		origRight: $('.theirs').html(),
-		highlightDifferences: true,
-		smartIndent: true,
-		mode: 'javascript',
-		lineNumbers: true
-	});
+var leftPanel = new CodeMirrorMerge.MergeView(document.getElementById('split-left'), {
+	value: $('.both').html(),
+	origLeft: $('.yours').html(),
+	origRight: $('.theirs').html(),
+	highlightDifferences: true,
+	smartIndent: true,
+	mode: 'javascript',
+	lineNumbers: true
+});
