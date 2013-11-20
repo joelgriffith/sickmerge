f89860328d461382d0d15fdeba53c77d00fcddcf
jgriffith <jgriffith@thetus.com>
2013-11-20 08:48:23 -0800

    Adding in command line options, re-organizing code to load in files when necessary

--- sickmerge.js
+++ sickmerge.js
@@ -11,52 +11,77 @@
  /*
  * Module Dependencies
  */
-var express = require('express'),
-    app = express(),
-    port = 2001,
-    fs = require('fs'),
-    path = require('path'),
-    exec = require('child_process').exec,
-    userArgs = process.argv.slice(2),
-	fileLocation = userArgs[0],
-	gitStrip = require('./lib/gitStrip');
+var fs = require('fs'),
+    program = require('commander');
+
+// Program Setup and Options
+program
+    .version('0.0.1')
+    .usage('[options] <file location ...>')
+    .option('-h, --hostname [value]', 'The hostname you wish to use (defaults to localhost)')
+    .option('-p, --port <n>', 'The port you wish to deploy on (defaults to 3000)', parseInt)
+    .option('-m, --merge [value]', 'What you want to display in the middle (merged) window on instantiation. Valid options are "yours", "theirs", and "both". Defaults to "yours"')
+    .parse(process.argv);
+
+// No File given, print the help
+if (!program.args[0]) {
+    program.outputHelp();
+    return;
+}
+
+// Invalid merge input
+if (['yours', 'theirs', 'both'].indexOf(program.merge) === -1) {
+    console.log('You specified an invalid merge result, please use either "yours", "theirs", or "both"');
+    return;
+}
 
 // Read the passed file, strip the git comments, and build the web service
-fs.readFile(fileLocation, 'UTF-8', function(err, result) {
-	if (err) throw 'There was a problem opening the file: ' + err;	
-	var resultArray = gitStrip(result);
-	/*
-	 * Application configuration and initiation
-	 */
-	app.use(express.bodyParser());
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'ejs');
+fs.readFile(program.args[0], 'UTF-8', function(err, result) {
+    if (err) return console.log('There was an error loading your file! ' + err);
 
-	/*
-	 * Serve the static public directory
-	 */
-	app.use(express.static(path.join(__dirname, 'public')));
+    // Setup parameters, load additional files
+    var hostname = (program.hostname) ? program.hostname : 'localhost',
+        port = (program.port) ? program.port : 3000,
+        merge = (program.merge) ? program.merge : 'yours',
+        gitStrip = require('./lib/gitStrip'),
+        resultArray = gitStrip(result, merge),
+        express = require('express'),
+        app = express(),
+        exec = require('child_process').exec,
+        path = require('path');    
 
-	// Route to retrive files on the system for editing
-	app.get('/', function (req, res) {
-		res.render('editor', { body: resultArray });
-	});
+    /*
+     * Application configuration and initiation
+     */
+    app.use(express.bodyParser());
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
 
-	// Route for persisting to the file system then closing out
-	app.post('/save', function (req) {
-		var location = '.' + req.body.location.replace('/file',''),
-			content = req.body.text;
+    /*
+     * Serve the static public directory
+     */
+    app.use(express.static(path.join(__dirname, 'public')));
 
-		fs.writeFile(location, content, function (err) {
-			if (err) throw err;
-		});
-	});
+    // Route to retrive files on the system for editing
+    app.get('/', function (req, res) {
+        res.render('editor', { body: resultArray });
+    });
 
-	/* 
-	 * Startup the server
-	 */
-	app.listen(port);
-	
-	// Open the browser to the page
-	exec('open http://localhost:' + port);
+    // Route for persisting to the file system then closing out
+    app.post('/save', function (req) {
+        var location = '.' + req.body.location.replace('/file',''),
+            content = req.body.text;
+
+        fs.writeFile(location, content, function (err) {
+            if (err) throw err;
+        });
+    });
+
+    /* 
+     * Startup the server
+     */
+    app.listen(port);
+    
+    // Open the browser to the page
+    exec('open http://' + hostname + ':' + port);
 });
