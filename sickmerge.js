1f78fe64100f4df021fafc14482c7325dd0f9a14
jgriffith <jgriffith@thetus.com>
2013-11-26 12:30:55 -0800

    Syntax options are now in including a command to show them

--- sickmerge.js
+++ sickmerge.js
@@ -12,6 +12,7 @@
  */
 var fs = require('fs'),
     program = require('commander'),
+    syntaxOptions = require('./lib/syntaxOptions'),
     fileLocation;
 
 // Program Setup and Options
@@ -19,13 +20,26 @@ program
     .version('0.0.1')
     .usage('[options] <conflicted file location>')
     .option('-h, --hostname [value]', 'The host URL you wish to query in the browser (defaults to localhost).')
+    .option('-o, --syntax-options', 'Will show the available syntax options for syntax highlighting.')
     .option('-p, --port <n>', 'The port you wish to deploy on (defaults to 3000).', parseInt)
-    .option('-s, --syntax [value]', 'The language of the file for syntax highlighting (optional), defaults to none.')
+    .option('-s, --syntax [value]', 'The language of the file for syntax highlighting (optional), defaults to no highlighting. Run with "-o" to see the available options.')
     .option('-m, --merge [value]', 'Specify the initial view in the middle (merged) window on instantiation. Valid options are "yours", "theirs", and "both". Defaults to "yours"')
     .parse(process.argv);
 
 // Store the file location so we can persist later
 fileLocation = program.args[0];
+
+// For printing available syntax options
+function printSyntaxOptions () {
+    console.log('Available options include:\n' + syntaxOptions);
+    return;
+}
+
+// If the user wants to see the syntax options
+if (program['syntax-options']) {
+    printSyntaxOptions();
+    return;
+}
 
 // No File given, print help since it's required
 if (!fileLocation) {
@@ -36,6 +50,13 @@ if (!fileLocation) {
 // Invalid merge option
 if (program.merge && ['yours', 'theirs', 'both'].indexOf(program.merge) === -1) {
     console.log('You\'ve specified an invalid initial merged view: "' + program.merge + '".\nPlease use either "yours", "theirs", or "both"');
+    return;
+}
+
+// Invalid syntax option
+if (program.syntax && syntaxOptions.indexOf(program.syntax) === -1) {
+    console.log('You\'ve specified an invalid syntax option: ' + program.syntax);
+    printSyntaxOptions();
     return;
 }
 
