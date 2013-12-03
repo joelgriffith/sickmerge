80c9720f799d349cec052a9c3dc752157bcece8d
jgriffith <jgriffith@thetus.com>
2013-12-03 10:01:24 -0800

    Syntax theme helper is now unit tested.

--- test/spec/syntax_spec.js
+++ test/spec/syntax_spec.js
@@ -1,6 +1,17 @@
-var syntax = require('../../lib/syntax');
+var syntax = require('../../lib/syntax'),
+    hasDuplicates = function(array) {
+        var valuesSoFar = {};
+        for (var i = 0; i < array.length; ++i) {
+            var value = array[i];
+            if (Object.prototype.hasOwnProperty.call(valuesSoFar, value)) {
+                return true;
+            }
+            valuesSoFar[value] = true;
+        }
+        return false;
+    };
 
-describe('Synax helper library', function() {
+describe('Syntax theme helper library', function() {
 
     // Generic
     it('should be in /lib/spec.js', function(done) {
@@ -8,15 +19,52 @@ describe('Synax helper library', function() {
         done();
     });
     it('should have an extension map object', function(done) {
-        expect(syntax.extensionMap).toEqual(jasmine.any(Object));
+        expect(syntax.extensions).toEqual(jasmine.any(Object));
         done();
     });
-    it('should have a get syntax helper', function(done) {
+    it('should have a get syntax theme helper', function(done) {
         expect(syntax.getSyntax).toEqual(jasmine.any(Function));
         done();
     });
-    it('should have a show supported syntaxes helper', function(done) {
+    it('should have a show supported syntax themes helper', function(done) {
         expect(syntax.showSupportedSyntaxes).toEqual(jasmine.any(Function));
         done();
+    });
+
+    describe('get syntax theme helper', function() {
+        it('should return an empty string when no extension is found', function(done) {
+            expect(syntax.getSyntax('notavailable')).toEqual('');
+            done();
+        });
+        it('should return an empty string when an invalid option is passed to it', function(done) {
+            expect(syntax.getSyntax({})).toEqual('');
+            expect(syntax.getSyntax(9)).toEqual('');
+            expect(syntax.getSyntax([])).toEqual('');
+            expect(syntax.getSyntax(true)).toEqual('');
+            expect(syntax.getSyntax(false)).toEqual('');
+            done();
+        });
+        it('should return syntax theme when a valid extension is passed', function(done) {
+            expect(syntax.getSyntax('js')).toEqual('javascript');
+            expect(syntax.getSyntax('php')).toEqual('php');
+            expect(syntax.getSyntax('css')).toEqual('css');
+            done();
+        });
+    });
+
+    describe('show supported syntaxes helper', function() {
+        var results;
+        beforeEach(function() {
+            results = syntax.showSupportedSyntaxes();
+        });
+        it('should return a comma-separated list of available themes', function(done) {
+            expect(results).toContain(',');
+            done();
+        });
+        it('should not have repeated themes in the output', function(done) {
+            var hasDuplicateThemes = hasDuplicates(results.split(', '));
+            expect(hasDuplicateThemes).toBe(false);
+            done();
+        });
     });
 });
