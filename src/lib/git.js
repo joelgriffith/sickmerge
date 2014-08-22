3525131ebc5696a461d16eaa43ce99270acdb647
Joel Griffith <jgriffith@appnexus.com>
2014-08-22 15:52:58 -0700

    `ls` => `git`

--- src/lib/git.js
+++ src/lib/git.js
@@ -4,7 +4,7 @@ var concat = require('concat-stream');
 
 function _getConflicted(cb) {
 	if (typeof cb !== 'function') throw new Error('#getConflicted expects a function callback');
-	var conflicted = spawn('ls', ['diff', '--name-only', '--diff-filter=U']);
+	var conflicted = spawn('git', ['diff', '--name-only', '--diff-filter=U']);
 	var result = concat(function(data) {
 		cb(data.toString());
 	});
